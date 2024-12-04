c2a79c4bdb013cfd1c4f898c721d8d6a52486c0b
Smirnov <smirnov.mykyta@gmail.com>
2024-12-04 08:45:09 +0100

    created function for search pokemon by id and name; creared function getFavoritePokemonList

--- src/helper/search.js
+++ src/helper/search.js
@@ -1,1 +1,11 @@
-// search function id/name
+// import {testArray} from "../Mocks/mock.js";
+
+// const allPokemon = testArray;
+
+export function searchPokemonById(id, allPokemon) {
+    return allPokemon.find((elem) => elem.id === id);
+}
+
+export function searchPokemonByName(name, allPokemon) {
+    return allPokemon.find((elem) => elem.name.toLowerCase() === name.toLowerCase());
+}
