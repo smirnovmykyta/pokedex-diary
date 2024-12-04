cc1d926e6ad4a812694c391f6a31ea8f314df6d4
Smirnov <smirnov.mykyta@gmail.com>
2024-12-04 10:48:59 +0100

    fix

--- src/Mocks/mock.js
+++ src/Mocks/mock.js
@@ -4,69 +4,97 @@ export const testArray = [
         name: 'bulbasaur',
         abilities: ['overgrow', 'chlorophyll'],
         types: ['grass', 'poison'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+        favorite: false,
     },
     {
         id: 2,
         name: 'ivysaur',
         abilities: ['overgrow', 'chlorophyll'],
         types: ['grass', 'poison'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png',
+        favorite: true,
     },
     {
         id: 3,
         name: 'venusaur',
         abilities: ['overgrow', 'chlorophyll'],
         types: ['grass', 'poison'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png',
+        favorite: true,
     },
     {
         id: 4,
         name: 'charmander',
         abilities: ['blaze', 'solar-power'],
         types: ['fire'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
+        favorite: false,
     },
     {
         id: 5,
         name: 'charmeleon',
         abilities: ['blaze', 'solar-power'],
         types: ['fire'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png',
+        favorite: true,
     },
     {
         id: 6,
         name: 'charizard',
         abilities: ['blaze', 'solar-power'],
         types: ['fire', 'flying'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png',
+        favorite: false,
     },
     {
         id: 7,
         name: 'squirtle',
         abilities: ['torrent', 'rain-dish'],
         types: ['water'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
+        favorite: true,
     },
     {
         id: 8,
         name: 'wartortle',
         abilities: ['torrent', 'rain-dish'],
         types: ['water'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png',
+        favorite: true,
     },
     {
         id: 9,
         name: 'blastoise',
         abilities: ['torrent', 'rain-dish'],
         types: ['water'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png',
+        favorite: false,
     },
     {
         id: 10,
         name: 'caterpie',
         abilities: ['shield-dust', 'run-away'],
         types: ['bug'],
-        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png'
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png',
+        favorite: false,
     }
 ];
+
+// export const testFavoriteCardList = [
+//     {
+//         id: '1'
+//     },
+//     {
+//         id: '3'
+//     },
+//     {
+//         id: '4'
+//     },
+//     {
+//         id: '5'
+//     },
+//     {
+//         id: '8'
+//     }
+//     ];
